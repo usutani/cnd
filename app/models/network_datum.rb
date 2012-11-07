2429c0923f0c0b82720fa0ff5fe1cc6bc0127203
Yasuhiro Usutani <y_usutani@me.com>
2012-11-07 15:49:34 +0900

    Add validation

--- app/models/network_datum.rb
+++ app/models/network_datum.rb
@@ -1,3 +1,5 @@
 class NetworkDatum < ActiveRecord::Base
   attr_accessible :day, :received, :sent
+  validates :received, :sent, presence: true
+  validates :received, :sent, numericality: { greater_than: 0 }
 end
